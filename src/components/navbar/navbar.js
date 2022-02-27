cc4daae0cf401209e3cb1da906f0879b684a8ae2
Mohamed Youssef Nafkha <46995112+Nafkha@users.noreply.github.com>
2022-02-27 11:26:37 +0100

    Added dropdown

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
   return (
     <ul className='main_Menu'>
         {props.navItems.map((item)=><li><a href='https://google.com'>{item.title}</a>
-        {item.dropdown_elements?<ul className='dropdown_Menu active'>{item.dropdown_elements.map(el=><div><li className='dropdown_element'>{el}</li><br/></div>)}</ul>:null}
+        {item.dropdown_elements?<ul className='dropdown_Menu'>{item.dropdown_elements.map(el=><li className='dropdown_element'>{el}</li>)}</ul>:null}
         </li>)}
 
 
